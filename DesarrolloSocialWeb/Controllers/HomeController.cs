cb39e7ae67211ca76ac6167dd258bb105494afbd
EfrainAS <efrain.mejias@alfasoluciones.com.mx>
2022-06-27 08:06:54 -0400

    inicio

---
+++ DesarrolloSocialWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DesarrolloSocialWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DesarrolloSocialWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
