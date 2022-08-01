3431093ebbf38bfaf6a693ec6e6571a6f2d1d25e
EfrainMejiasC <efrainmejiasc@hotmail.com>
2022-08-01 16:56:27 -0400

    Update

--- DesarrolloSocialWeb/Controllers/HomeController.cs
+++ DesarrolloSocialWeb/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using DesarrolloSocialWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace DesarrolloSocialWeb.Controllers
 {
@@ -37,6 +40,62 @@ namespace DesarrolloSocialWeb.Controllers
         public IActionResult CargaFamiliar()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPaisesAsync()
+        {
+            var respuesta = string.Empty;
+            var paises = new PaisesModel();
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://restcountries.com/v3.1/all");
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://countriesnow.space/api/v0.1/countries");
+                var response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    respuesta = await response.Content.ReadAsStringAsync();
+                    paises = JsonConvert.DeserializeObject<PaisesModel>(respuesta);
+                }
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message;
+            }
+
+            return Json(paises);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCiudadesPaisAsync(int id)
+        {
+            var respuesta = string.Empty;
+            var paises = new PaisesModel();
+            var ciudades = new List<string>();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "https://countriesnow.space/api/v0.1/countries");
+                var response = await client.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    respuesta = await response.Content.ReadAsStringAsync();
+                    paises = JsonConvert.DeserializeObject<PaisesModel>(respuesta);
+                    ciudades = paises.data[id].cities.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message;
+            }
+
+            return Json(ciudades);
         }
 
 
---
+++ DesarrolloSocialWeb/Models/PaisesModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DesarrolloSocialWeb.Models
+{
+    public class PaisesModel
+    {
+        public Name? name { get; set; }
+
+        public bool error { get; set; }
+        public string msg { get; set; }
+        public List<Datum> data { get; set; }
+    }
+
+    public class Name
+    {
+        public string? common { get; set; }
+        public string? official { get; set; }
+        //public NativeName nativeName { get; set; }
+    }
+    public class Datum
+    {
+        public string iso2 { get; set; }
+        public string iso3 { get; set; }
+        public string country { get; set; }
+        public List<string> cities { get; set; }
+    }
+
+}
